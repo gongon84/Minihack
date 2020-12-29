fc3c7405d4a05ec696a3cab168d1fa6ba36dd421
gongon84 <shinya7102@gmail.com>
2020-12-30 02:31:59 +0900

    UI改善, fontawesome追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/" => "home#index"
+  get "/test" => "home#test"
 
   # events
   get "/events/index" => "events#index"
