741cb6dfbea43ee52e9e7c21b11186e7fb733e63
gongon84 <shinya7102@gmail.com>
2021-07-23 13:32:30 +0900

    いいね数並び替え機能　作成

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,4 +6,10 @@ class Work < ApplicationRecord
     def event
         return Event.find_by(id: self.event_id)
     end
+
+    # いいねカウントをするため
+    ransacker :likes_count do
+        query = '(SELECT COUNT(likes.work_id) FROM likes where likes.work_id = works.id GROUP BY likes.work_id)'
+        Arel.sql(query)
+    end
 end
